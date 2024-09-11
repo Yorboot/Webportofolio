a86321d3b3d161694326bfebfa2542a36718517f
royboot <rgrizell467@gmail.com>
2024-09-12 00:05:22 +0200

    Back end
    Back end

--- includes/Ph.inc.php
+++ includes/Ph.inc.php
@@ -13,16 +13,20 @@ function Cinput(string $data): string
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(isset($_POST["Title"])&&isset($_POST["Description"])&&isset($_POST["image"])){
         try {
+
             $Title = Cinput($_POST["Title"]);
             $Description = Cinput($_POST["Description"]);
             $Link = $_POST["Link"];
             $img = $_POST["image"];
-            $image = file_get_contents($_FILES[$img]['tmp_name']);
-            $stmt = $pdo ->query("INSERT INTO Projects (Title, Description,Image,Link) VALUES (':Title', ':Description', ':image',':Link')");
+            foreach ($_FILES as $value) {
+                echo $value;
+            }
+            // $image = file_get_contents($_FILES[$img]);
+            $stmt = $pdo ->query("INSERT INTO Projects (Titel, Description,Image,Link) VALUES (':Title', ':Description', ':image',':Link')");
             $stmt -> bindParam(':Title', $Title);
             $stmt -> bindParam(':Description', $Description);
-            $stmt -> bindParam(':image', $image,PDO::PARAM_LOB);
-            $stmt->bindParam(':Link', $link);
+            // $stmt -> bindParam(':image', $image,PDO::PARAM_LOB);
+            $stmt -> bindParam(':Link', $link);
             $stmt -> execute();
             header("Location:../Contact.html");
             exit();
